2c4f80e8f9e4cc6bac7445b179a80c5fa000bc7b
worksofallwin <princeallwin199@gmail.com>
2024-02-28 17:53:50 +0530

    write horseStore in yul

--- src/horseStoreV1/HorseStoreYul.sol
+++ src/horseStoreV1/HorseStoreYul.sol
@@ -9,14 +9,30 @@ contract HorseStoreYul is IHorseStore {
     function updateHorseNumber(uint256 newNumberOfHorses) external {
         assembly {
             sstore(numberOfHorses.slot, newNumberOfHorses)
+            // storing `newNumberOfHorses` in slot of `numberOfHorses`
         }
     }
 
     function readNumberOfHorses() external view returns (uint256) {
         assembly {
-            let num := sload(numberOfHorses.slot) // := means set // read from storage and set it in `num`
-            mstore(0, num) // to return we have to store the value in memory => slot is `0` and value is `num`
-            return(0, 0x20) // value will be at slot 0 and 0x20 is the size of the return type
+            let num := sload(numberOfHorses.slot)
+            // := means set
+            // load from `storage slot` and set it in `num`
+
+            mstore(0, num)
+            // to return we have to store the value in memory => slot is `0` and value is `num`
+
+            return(0, 0x20)
+            // value will be at `slot 0` and `0x20` is the size of the return type
         }
     }
 }
+
+// Yul is little different than working with huff
+// when we call `mstore` in huff
+// we are expecting `offset` and `value` to be already on the stack
+// but in Yul, we are giving it as a parameter
+// mstore(0, num)
+// which means `offset` is at the top of the stack which is `0`
+// Right underneath is the `value` which is `num`
+// Yul will push `offset` and `value` on the stack and execute
